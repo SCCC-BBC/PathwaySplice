9ebd5fc059eb382c4152d28e3eabd3aa75607f3e
aiminy <jlfmssm@gmail.com>
2016-03-03 10:31:43 -0500

    Update run juctionsSeq

--- R/Run_get_results_from_JunctionSeq.R
+++ R/Run_get_results_from_JunctionSeq.R
@@ -88,55 +88,55 @@ capture.output(for(go in Re.Go.adjusted.by.number.junction.sampling.based[[3]]$c
 Re.Go.adjusted.by.number.exon.junction<-GotermAnalysis2GetAllGOTerms_AdjustedByNumOfJunctionWithinOneGene(data.table.gene.based.all.jscs4,ad="EJ",NULL,0.05,"Splice_exon_based_2_25_2016")
 data.pwf2.exons.junction<-plotPWF2(Re.Go.adjusted.by.number.exon.junction[[2]],binsize=30,xlab = "Number of exons and splice junctions(<binsize> gene bins)")
 
-#KEGG enrichment analysis
-#adjust by the number of splice junction
-Re.Go.adjusted.by.number.junction.sampling.based.kegg<-GotermAnalysis2GetAllGOTerms_AdjustedByNumOfJunctionWithinOneGene_KEGG(data.table.gene.based.all.jscs4,ad="J","J",0.05,"Splice_junction_based_2_26_2016_2")
-data.pwf2.SJs.sampling<-plotPWF2(Re.Go.adjusted.by.number.junction.sampling.based.kegg[[2]],binsize=30,xlab = "Number of SJs(<binsize> gene bins)")
-
-#adjust by the number of exons
-Re.Go.adjusted.by.number.exon.sampling.based.kegg<-GotermAnalysis2GetAllGOTerms_AdjustedByNumOfJunctionWithinOneGene_KEGG(data.table.gene.based.all.jscs4,ad="E","E",0.05,"Exon_based_2_26_2016")
-data.pwf2.exons.sampling<-plotPWF2(Re.Go.adjusted.by.number.exon.sampling.based[[2]],binsize=30,xlab = "Number of exons(<binsize> gene bins)")
-
-#adjust by the number of exons and splice junctions
-Re.Go.adjusted.by.number.exon.junction<-GotermAnalysis2GetAllGOTerms_AdjustedByNumOfJunctionWithinOneGene_KEGG(data.table.gene.based.all.jscs4,ad="EJ",NULL,0.05,"Splice_exon_based_2_26_2016")
-data.pwf2.exons.junction<-plotPWF2(Re.Go.adjusted.by.number.exon.junction[[2]],binsize=30,xlab = "Number of exons and splice junctions(<binsize> gene bins)")
-
-#Anotate KEGG pathway
-keggid2keggname <- as.list(KEGGPATHID2NAME)
-AddKEGGAnnotation<-function(Data2BeAnno,keggName,file_prefix){
-
-keggid2<-Data2BeAnno[[1]][,1]
-Data2BeAnno.annotation<-as.data.frame(unlist(keggid2keggname[keggid2]))
-Data2BeAnno.annotation.2<-cbind(rownames(Data2BeAnno.annotation),as.character(Data2BeAnno.annotation[,1]))
-colnames(Data2BeAnno.annotation.2)=c("category","annotation")
-head(Data2BeAnno.annotation.2)
-head(Data2BeAnno[[1]])
-Data2BeAnno.anno<-merge(Data2BeAnno[[1]],Data2BeAnno.annotation.2,by=1)
-
-keggid3<-Data2BeAnno[[3]][,1]
-Data2BeAnno.annotation.enriched<-as.data.frame(unlist(keggid2keggname[keggid3]))
-Data2BeAnno.annotation.enriched.2<-cbind(rownames(Data2BeAnno.annotation.enriched),as.character(Data2BeAnno.annotation.enriched[,1]))
-colnames(Data2BeAnno.annotation.enriched.2)=c("category","annotation")
-head(Data2BeAnno.annotation.2)
-head(Data2BeAnno[[1]])
-Data2BeAnno.anno.enriched<-merge(Data2BeAnno[[3]],Data2BeAnno.annotation.enriched.2,by=1)
-
-write.csv(Data2BeAnno.anno,file=paste(file_prefix,"_all_KEGG",".csv",sep=""),row.names = FALSE)
-write.csv(Data2BeAnno.anno.enriched,file=paste(file_prefix,"_enriched_KEGG",".csv",sep=""),row.names = FALSE)
-
-}
-
-#Add annotations to the KEGG enrichment analysis adjusted by the number of splice junctions
-AddKEGGAnnotation(Re.Go.adjusted.by.number.junction.sampling.based.kegg,keggid2keggname,"Splice_junction_based")
-
-#Add annotations to the KEGG enrichment analysis adjusted by the number of exons
-AddKEGGAnnotation(Re.Go.adjusted.by.number.exon.sampling.based.kegg,keggid2keggname,"Exon_junction_based")
-
-#Add annotations to the KEGG enrichment analysis adjusted by the number of splice junctions and exons
-AddKEGGAnnotation(Re.Go.adjusted.by.number.exon.junction,keggid2keggname,"Splice_junction_exon_based")
-
-#Re.Go.adjusted.by.number.exon.junction[[2]]
-#goseq.m(Re.Go.adjusted.by.number.exon.junction[[2]],"mm10","ensGene",method = "Sampling",repcnt=10000,test.cats = c("KEGG"))
-
-#test.KEGG<-as.data.frame(unlist(getgo.m(rownames(Re.Go.adjusted.by.number.exon.junction[[2]]),"mm10","ensGene",fetch.cats=c("KEGG"))))
-#test.KEGG<-getgo.m(rownames(Re.Go.adjusted.by.number.exon.junction[[2]]),"mm10","ensGene",fetch.cats=c("KEGG"))
+# #KEGG enrichment analysis
+# #adjust by the number of splice junction
+# Re.Go.adjusted.by.number.junction.sampling.based.kegg<-GotermAnalysis2GetAllGOTerms_AdjustedByNumOfJunctionWithinOneGene_KEGG(data.table.gene.based.all.jscs4,ad="J","J",0.05,"Splice_junction_based_2_26_2016_2")
+# data.pwf2.SJs.sampling<-plotPWF2(Re.Go.adjusted.by.number.junction.sampling.based.kegg[[2]],binsize=30,xlab = "Number of SJs(<binsize> gene bins)")
+#
+# #adjust by the number of exons
+# Re.Go.adjusted.by.number.exon.sampling.based.kegg<-GotermAnalysis2GetAllGOTerms_AdjustedByNumOfJunctionWithinOneGene_KEGG(data.table.gene.based.all.jscs4,ad="E","E",0.05,"Exon_based_2_26_2016")
+# data.pwf2.exons.sampling<-plotPWF2(Re.Go.adjusted.by.number.exon.sampling.based[[2]],binsize=30,xlab = "Number of exons(<binsize> gene bins)")
+#
+# #adjust by the number of exons and splice junctions
+# Re.Go.adjusted.by.number.exon.junction<-GotermAnalysis2GetAllGOTerms_AdjustedByNumOfJunctionWithinOneGene_KEGG(data.table.gene.based.all.jscs4,ad="EJ",NULL,0.05,"Splice_exon_based_2_26_2016")
+# data.pwf2.exons.junction<-plotPWF2(Re.Go.adjusted.by.number.exon.junction[[2]],binsize=30,xlab = "Number of exons and splice junctions(<binsize> gene bins)")
+#
+# #Anotate KEGG pathway
+# keggid2keggname <- as.list(KEGGPATHID2NAME)
+# AddKEGGAnnotation<-function(Data2BeAnno,keggName,file_prefix){
+#
+# keggid2<-Data2BeAnno[[1]][,1]
+# Data2BeAnno.annotation<-as.data.frame(unlist(keggid2keggname[keggid2]))
+# Data2BeAnno.annotation.2<-cbind(rownames(Data2BeAnno.annotation),as.character(Data2BeAnno.annotation[,1]))
+# colnames(Data2BeAnno.annotation.2)=c("category","annotation")
+# head(Data2BeAnno.annotation.2)
+# head(Data2BeAnno[[1]])
+# Data2BeAnno.anno<-merge(Data2BeAnno[[1]],Data2BeAnno.annotation.2,by=1)
+#
+# keggid3<-Data2BeAnno[[3]][,1]
+# Data2BeAnno.annotation.enriched<-as.data.frame(unlist(keggid2keggname[keggid3]))
+# Data2BeAnno.annotation.enriched.2<-cbind(rownames(Data2BeAnno.annotation.enriched),as.character(Data2BeAnno.annotation.enriched[,1]))
+# colnames(Data2BeAnno.annotation.enriched.2)=c("category","annotation")
+# head(Data2BeAnno.annotation.2)
+# head(Data2BeAnno[[1]])
+# Data2BeAnno.anno.enriched<-merge(Data2BeAnno[[3]],Data2BeAnno.annotation.enriched.2,by=1)
+#
+# write.csv(Data2BeAnno.anno,file=paste(file_prefix,"_all_KEGG",".csv",sep=""),row.names = FALSE)
+# write.csv(Data2BeAnno.anno.enriched,file=paste(file_prefix,"_enriched_KEGG",".csv",sep=""),row.names = FALSE)
+#
+# }
+#
+# #Add annotations to the KEGG enrichment analysis adjusted by the number of splice junctions
+# AddKEGGAnnotation(Re.Go.adjusted.by.number.junction.sampling.based.kegg,keggid2keggname,"Splice_junction_based")
+#
+# #Add annotations to the KEGG enrichment analysis adjusted by the number of exons
+# AddKEGGAnnotation(Re.Go.adjusted.by.number.exon.sampling.based.kegg,keggid2keggname,"Exon_junction_based")
+#
+# #Add annotations to the KEGG enrichment analysis adjusted by the number of splice junctions and exons
+# AddKEGGAnnotation(Re.Go.adjusted.by.number.exon.junction,keggid2keggname,"Splice_junction_exon_based")
+#
+# #Re.Go.adjusted.by.number.exon.junction[[2]]
+# #goseq.m(Re.Go.adjusted.by.number.exon.junction[[2]],"mm10","ensGene",method = "Sampling",repcnt=10000,test.cats = c("KEGG"))
+#
+# #test.KEGG<-as.data.frame(unlist(getgo.m(rownames(Re.Go.adjusted.by.number.exon.junction[[2]]),"mm10","ensGene",fetch.cats=c("KEGG"))))
+# #test.KEGG<-getgo.m(rownames(Re.Go.adjusted.by.number.exon.junction[[2]]),"mm10","ensGene",fetch.cats=c("KEGG"))
