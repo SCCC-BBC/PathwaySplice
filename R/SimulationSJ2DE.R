2a69d486b37bf0c624e8ad7a8a1b457911c8668b
aiminy <jlfmssm@gmail.com>
2016-05-13 12:04:28 -0400

    change title,description,input parameters for SimulationSJ2DE function

--- R/SimulationSJ2DE.R
+++ R/SimulationSJ2DE.R
@@ -1,13 +1,25 @@
-#' Title
+#' @title Simulate gene sets based on the number of splicing junctions
+#
+#' @description
+#'We sample the mean of the number of splicing junction, and use this mean to simulate an array A including
+#'splicing junction numbers with certain amounts. we convert this number into an array AAA between 0 and 1. Use the median of
+#'AAA array as the probability for 1, we sample from [0,1] to get a list with certain length. This list is used as
+#'a gene set. For example, if this gene set has 30 genes, each gene has its splicing junction number, we will
+#'have 30 splicing junction number. After differential gene expression analysis, some genes are differentiallly expressed,
+#'and are labeled as 1,otherwise labeled as 0.
+#'Here we simulate two scenarios:
+#'Scenario1: the probability of being 1 is dependent on the median of AAA
+#'Scenario2: the probability of being 1 is not dependent on the median of AAA
 #'
-#' @param min_num_splicing
-#' @param max_num_splicing
-#' @param num_gene
+#' @param min_num_splicing minium splicing junction number
+#' @param max_num_splicing max splicing junction number
+#' @param num_gene number of gene
 #'
-#' @return
+#' @return a list with two set of genes
 #' @export
-#'
 #' @examples
+#' re.random.DE2SJ<-SimulationSJ2DE(20,200,30)
+
 SimulationSJ2DE<- function(min_num_splicing,max_num_splicing,num_gene) {
   crr.random<-array()
   for(j in 1:1:100){
