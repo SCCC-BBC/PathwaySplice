aff5883e8384c6ff449483553f74e97f59671fb2
aiminy <jlfmssm@gmail.com>
2016-06-15 15:48:19 -0400

    remove unused codes in gene2cat

--- R/gene2cat.R
+++ R/gene2cat.R
@@ -1,3 +1,5 @@
+#' gene2cat
+#'
 #' Given a gene name and the object built from reading Gmt file, and find the pathways that this gene corresponds to
 #'
 #' @param gene_name
@@ -7,19 +9,12 @@
 #' @export
 #'
 #' @examples
+#'
+#'
 gene2cat <- function(gene_name,re) {
-
-  #length(gene.name)
-  #which(gene.name==gene_name)
   z<-re$genesets
-
   res <- lapply(z, function(ch) grep(gene_name, ch))
   res2<-sapply(res, function(x) length(x) > 0)
-  #re$geneset.names[res2]
-
   gene2cat<-list(re$geneset.names[res2])
-
-  #names(gene2cat)<-gene_name
-
   gene2cat
 }
