d10f9b24821932f1c9580e72dbf495de712276a2
aiminy <jlfmssm@gmail.com>
2016-06-15 15:33:13 -0400

    remove unused examples and blank lines

--- R/Gmt2GeneCat.R
+++ R/Gmt2GeneCat.R
@@ -1,4 +1,6 @@
-#' Read a gmt file, return a list with the name of element being a gene id based on gene_anno_file, and each element
+#' Gmt2GeneCat
+#'
+#' Read a gmt file, and return a list with the name of element being a gene id based on gene_anno_file, and each element
 #' being the pathways that this gene corresponds to
 #'
 #' @param gmt_input_file
@@ -10,31 +12,16 @@
 #'
 #' @examples
 #'
-#' gene.2.cat.hallmark<-Gmt2GeneCat("/media/H_driver/2015/Nimer_Cheng/h.all.v5.1.symbols.gmt",
-#' "/media/H_driver/Annotation/mm10/genes_table_02052016.csv")
-#'
-#' gene.2.cat.hallmark<-Gmt2GeneCat("/media/H_driver/2015/Nimer_Cheng/h.all.v5.1.symbols.gmt",
-#' "/media/H_driver/Annotation/hg38/genes_table_02092016.csv")
-#'
-#'
 #' gene.2.cat.cp.mouse<-Gmt2GeneCat("/media/H_driver/Annotation/MsigDB/c2.cp.Mouse.v5.1.symbols.gmt",
 #' "/media/H_driver/Annotation/mm10/genes_table_02052016.csv")
 #'
 #' gene.2.cat.tft.mouse<-Gmt2GeneCat("/media/H_driver/Annotation/MsigDB/c3.tft.Mouse.v5.1.symbols.gmt",
 #' "/media/H_driver/Annotation/mm10/genes_table_02052016.csv")
 #'
-#'
-#'
-#'
-#'
-#'
-#'
-#'
-#'
+
 Gmt2GeneCat <- function(gmt_input_file,gene_anno_file) {
 
   gene.2.cat.gmt<-gene2cat2(gmt_input_file)
-  #length(gene.2.cat.gmt)
 
   names.gene.gmt<-as.data.frame(names(gene.2.cat.gmt))
   colnames(names.gene.gmt)<-"gene_id"
@@ -44,9 +31,7 @@ Gmt2GeneCat <- function(gmt_input_file,gene_anno_file) {
   human.gene.ID.conversion.2<-human.gene.ID.conversion[names.gene.gmt.2,]
   gene.2.cat.gmt.2<-gene.2.cat.gmt
 
-  #if(based_by=="ensGene"){
   names(gene.2.cat.gmt.2)<-human.gene.ID.conversion.2[,3]
-  #}
 
   gene.2.cat.gmt.2
 }
