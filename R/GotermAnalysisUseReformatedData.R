193551902aca00f7f941f1d8ea3b415f0b375c3c
aiminy <jlfmssm@gmail.com>
2016-10-09 15:26:41 -0400

    remove old data, and add new example

--- R/GotermAnalysisUseReformatedData.R
+++ R/GotermAnalysisUseReformatedData.R
@@ -11,15 +11,9 @@
 #' @export
 #'
 #' @examples
-#'
-#'
-#' Re.Go.adjusted.by.number.junction.2<-GotermAnalysis2GetAllGOTerms_AdjustedByNumOfJunctionWithinOneGene(
-#' re.PJ.gene.based,ad="J","J",0.05,"Splice_junction_based")
-#'
-#' data.pwf2.SJs<-plotPWF2(Re.Go.adjusted.by.number.junction.2[[2]],binsize=30,xlab = "Number of SJs(<binsize> gene bins)")
-#'
-#' Re.Go.adjusted.by.exon.SJ<-GotermAnalysis2GetAllGOTerms_AdjustedByNumOfJunctionWithinOneGene(
-#' re.PJ.gene.based,ad="exon_SJ",sub_feature=NULL,0.05,file_prefix="Exon_Splice_junction_based.xls",gene_model=gene.model)
+#' data(mds)
+#' data(hg19)
+#' Example.Go.adjusted.by.exon<-GotermAnalysisUseReformatedData(mds,ad="exon_SJ",sub_feature="E",0.05,genomeID="hg19",geneID="ensGene",gene_model=hg19.gene.model)
 #'
 GotermAnalysisUseReformatedData<-function(re.gene.based,ad="GL",sub_feature=NULL,threshold,genomeID,geneID,gene_model){
 
@@ -33,8 +27,11 @@ GotermAnalysisUseReformatedData<-function(re.gene.based,ad="GL",sub_feature=NULL
 
   #print(dim(Data4Goterm.sub_feature))
 
+  if(sub_feature=="J"){
   Data4Goterm.sub_feature.geneID.NumOfJunctions<-Data4Goterm.sub_feature[,c(1,11)]
-
+  }else{
+    Data4Goterm.sub_feature.geneID.NumOfJunctions<-Data4Goterm.sub_feature[,c(1,10)]
+  }
   #print(dim(Data4Goterm.sub_feature.geneID.NumOfJunctions))
 
   Data4Goterm.sub_feature.Sig<-Data4Goterm.sub_feature[which(Data4Goterm.sub_feature[,7]<threshold),]
@@ -75,7 +72,7 @@ GotermAnalysisUseReformatedData<-function(re.gene.based,ad="GL",sub_feature=NULL
   GO.wall.DE_interest=goseq2(pwf.DE_interest,genomeID,geneID,gene.model=gene_model,use_genes_without_cat=TRUE)
 
   #GO.wall.DE_interest=goseq2(pwf.DE_interest,"mm10","ensGene",gene.model=gene_model)
-  enriched.GO.DE_interest=GO.wall.DE_interest[p.adjust(GO.wall.DE_interest$over_represented_pvalue,method="BH")<threshold,]
+  #enriched.GO.DE_interest=GO.wall.DE_interest[p.adjust(GO.wall.DE_interest$over_represented_pvalue,method="BH")<threshold,]
   re<-list()
 
   re[[1]]<-GO.wall.DE_interest
