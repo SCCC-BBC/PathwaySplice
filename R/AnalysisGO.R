1d0696752a79be0d67c5f5e4b16ae3a809da7ba9
aiminy <jlfmssm@gmail.com>
2016-10-13 11:31:08 -0400

    remove unused codes

--- R/AnalysisGO.R
+++ R/AnalysisGO.R
@@ -1,21 +1,21 @@
 AnalysisGO <- function() {
-  library(org.Hs.eg.db)
-  data(geneList)
-  gene <- names(geneList)[abs(geneList) > 2]
-  gene.df <- bitr(gene, fromType = "ENTREZID", 
-                  toType = c("ENSEMBL", "SYMBOL"),
-                  OrgDb = org.Hs.eg.db)
-  
-  ego <- enrichGO(gene          = gene,
-                  universe      = names(geneList),
-                  OrgDb         = org.Hs.eg.db,
-                  ont           = "CC",
-                  pAdjustMethod = "BH",
-                  pvalueCutoff  = 0.01,
-                  qvalueCutoff  = 0.05)
-  
-  head(summary(ego))
-  
+  # library(org.Hs.eg.db)
+  # data(geneList)
+  # gene <- names(geneList)[abs(geneList) > 2]
+  # gene.df <- bitr(gene, fromType = "ENTREZID", 
+  #                 toType = c("ENSEMBL", "SYMBOL"),
+  #                 OrgDb = org.Hs.eg.db)
+  # 
+  # ego <- enrichGO(gene          = gene,
+  #                 universe      = names(geneList),
+  #                 OrgDb         = org.Hs.eg.db,
+  #                 ont           = "CC",
+  #                 pAdjustMethod = "BH",
+  #                 pvalueCutoff  = 0.01,
+  #                 qvalueCutoff  = 0.05)
+  # 
+  # head(summary(ego))
+  # 
   gene2<-row.names(Example.Go.adjusted.by.exon[[2]][which(Example.Go.adjusted.by.exon[[2]][,1]==1),])
   
   gene.df <- bitr(gene2, fromType = "ENSEMBL", 
@@ -29,8 +29,8 @@ AnalysisGO <- function() {
                    pAdjustMethod = "BH",
                    pvalueCutoff  = 0.5,
                    qvalueCutoff  = 0.5)
-  head(summary(ego2))
   
+  head(summary(ego2))
   dotplot(ego2, showCategory=30)
   
   ego3 <- enrichGO(gene         = gene.df$SYMBOL,
@@ -40,11 +40,9 @@ AnalysisGO <- function() {
                    pAdjustMethod = "BH",
                    pvalueCutoff  = 0.5,
                    qvalueCutoff  = 0.5)
+  
   head(summary(ego3))
-  
   enrichMap(ego3, vertex.label.cex=1.2, layout=igraph::layout.kamada.kawai)
-  
   cnetplot(ego3)
-  
   plotGOgraph(ego2)
 }
