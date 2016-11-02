d72c2560bba48e870b02d601b847428807f159d2
aiminy <jlfmssm@gmail.com>
2016-11-02 15:45:21 -0400

    add enMap2 function

--- R/testEnrichMap.R
+++ R/testEnrichMap.R
@@ -13,12 +13,18 @@ testEnrichMap <- function() {
   #data(package="PathwaySplice")
   #data(geneList)
   data(mds)
-  Example.Go.adjusted.by.exon<-GotermAnalysisUseReformatedData(mds,ad="exon_SJ",sub_feature="E",0.05,genomeID="hg19",geneID="ensGene",gene_model=hg19.gene.model,method="Wallenius")
   
+  
+  #Analysis
+  Example.Go.adjusted.by.exon<-Run_pathwaysplice(mds,ad="exon_SJ",sub_feature="E",0.05,genomeID="hg19",geneID="ensGene",gene_model=hg19.gene.model,method="Wallenius")
+  
+  Example.Go.unadjusted<-Run_pathwaysplice(mds,ad="exon_SJ",sub_feature="E",0.05,genomeID="hg19",geneID="ensGene",gene_model=hg19.gene.model,method="Hypergeometric")
+  
+  #Check bias
+  plotPWF2(Example.Go.adjusted.by.exon[[2]])
   #gene <- names(geneList)[abs(geneList) > 2]
   
   DE.index<-which(Example.Go.adjusted.by.exon[[2]]$DEgenes == 1)
-  
   gene<-unique(rownames(Example.Go.adjusted.by.exon[[2]][DE.index,]))
   
   gene.df <- bitr(gene, fromType = "ENSEMBL", 
@@ -38,8 +44,10 @@ testEnrichMap <- function() {
                      pAdjustMethod = "BH",
                      pvalueCutoff  = 1,
                      qvalueCutoff  = 0.8,GOFromGOSeq=Example.Go.adjusted.by.exon,whichway = "HT")
-
   dim(ego.HT@result)
+
+    re.HT<-enMap(ego.HT, vertex.label.cex=1,layout=igraph::layout.kamada.kawai,SimilarityThreshold=0.1)
+  
   #sink()
 
   #sink("test_ok_WHT.txt")
@@ -57,11 +65,11 @@ testEnrichMap <- function() {
   #sink()
   
   par(mfrow=c(2,1))
-
   #enrichMap(ego.HT, vertex.label.cex=1.2, layout=igraph::layout.kamada.kawai)
   #enrichMap(ego.WHT, vertex.label.cex=1.2, layout=igraph::layout.kamada.kawai)
 
-  re.HT<-enrichMap.test(ego.HT, vertex.label.cex=1, layout=igraph::layout.kamada.kawai)
-  re.WHT<-enrichMap.test(ego.WHT, vertex.label.cex=1, layout=igraph::layout.kamada.kawai,SimilarityThreshold=0.1)
+  re.WHT<-enMap(ego.WHT, vertex.label.cex=1, layout=igraph::layout.kamada.kawai,SimilarityThreshold=0.1)
   
+  re.w1<-enMap2(Example.Go.adjusted.by.exon,n=5,SimilarityThreshold=0)
+  re.w2<-enMap2(Example.Go.unadjusted,n=5,SimilarityThreshold=0)
 }
