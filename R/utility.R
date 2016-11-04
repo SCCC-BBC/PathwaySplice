2720d52231cd2ad05009d3cfbb8d918bc1c4565b
aiminy <jlfmssm@gmail.com>
2016-11-04 14:19:36 -0400

    add new function

--- R/utility.R
+++ R/utility.R
@@ -44,3 +44,22 @@ ReformatData <- function(re.PJ.gene.based) {
   return(re3)
   
 }
+
+# @examples
+# dir.name="/media/H_driver/2016/Yang/MACS/MACS/"
+# reformatPath(dir.name) 
+
+reformatPath <- function(dir.name){
+  
+  CheckOPS=Sys.info()[['sysname']]
+  
+  if(CheckOPS=="Darwin"){
+    
+    temp=unlist(strsplit(dir.name,split="\\/"))
+    temp[2]="Volumes"
+    temp[3]="Bioinformatics$"
+    dir.name=paste0(paste0(temp,collapse = "/"),"/")
+  }
+  
+  return(dir.name)
+}
