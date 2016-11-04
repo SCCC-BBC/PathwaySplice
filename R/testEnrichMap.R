5be0fe8d2b88a714589426d4f713e6dde33d7e3c
aiminy <jlfmssm@gmail.com>
2016-11-04 15:38:54 -0400

    remove unused codes

--- R/testEnrichMap.R
+++ R/testEnrichMap.R
@@ -6,12 +6,9 @@ testEnrichMap <- function() {
   library(gdata)
   library(GO.db)
   library(DOSE,quietly = TRUE)
-  #library(clusterProfiler,quietly = TRUE)
   library(reshape2)
   library(igraph)
   
-  #data(package="PathwaySplice")
-  #data(geneList)
   data(mds)
   
   #Analysis
@@ -23,56 +20,8 @@ testEnrichMap <- function() {
   plotPWF2(Example.Go.adjusted.by.exon[[2]])
   #gene <- names(geneList)[abs(geneList) > 2]
   
+  #Construct network between gene sets
   re.w.adjusted<-enMap2(Example.Go.adjusted.by.exon,n=4,SimilarityThreshold=0)
-  
   re.w.unadjusted<-enMap2(Example.Go.unadjusted,n=4,SimilarityThreshold=0)
   
-  # DE.index<-which(Example.Go.adjusted.by.exon[[2]]$DEgenes == 1)
-  # gene<-unique(rownames(Example.Go.adjusted.by.exon[[2]][DE.index,]))
-  # 
-  # gene.df <- bitr(gene, fromType = "ENSEMBL", 
-  #                 toType = c("ENTREZID", "SYMBOL"),
-  #                 OrgDb = org.Hs.eg.db)
-  # 
-  # head(gene.df)
-  # 
-  #sink("test_ok_HT.txt")
-  #Use modified enrichGO
-  
-  # ego.HT <- enrichGO(gene          = gene,
-  #                    universe      = rownames(Example.Go.adjusted.by.exon[[2]]),
-  #                    keytype = "ENSEMBL",
-  #                    OrgDb         = org.Hs.eg.db,
-  #                    ont           = "BP",
-  #                    pAdjustMethod = "BH",
-  #                    pvalueCutoff  = 1,
-  #                    qvalueCutoff  = 0.8,GOFromGOSeq=Example.Go.adjusted.by.exon,whichway = "HT")
-  # dim(ego.HT@result)
-  # 
-  #   re.HT<-enMap(ego.HT, vertex.label.cex=1,layout=igraph::layout.kamada.kawai,SimilarityThreshold=0.1)
-  # 
-  #sink()
-
-  #sink("test_ok_WHT.txt")
-  
-  # ego.WHT <- enrichGO(gene          = gene,
-  #                     universe      = rownames(Example.Go.adjusted.by.exon[[2]]),
-  #                     keytype = "ENSEMBL",
-  #                     OrgDb         = org.Hs.eg.db,
-  #                     ont           = "BP",
-  #                     pAdjustMethod = "BH",
-  #                     pvalueCutoff  = 1,
-  #                     qvalueCutoff  = 0.8,GOFromGOSeq=Example.Go.adjusted.by.exon,whichway = "WHT")
-  # 
-  # dim(ego.WHT@result)
-  # #sink()
-  # 
-  # par(mfrow=c(2,1))
-  # #enrichMap(ego.HT, vertex.label.cex=1.2, layout=igraph::layout.kamada.kawai)
-  # #enrichMap(ego.WHT, vertex.label.cex=1.2, layout=igraph::layout.kamada.kawai)
-  # 
-  # re.WHT<-enMap(ego.WHT, vertex.label.cex=1, layout=igraph::layout.kamada.kawai,SimilarityThreshold=0.1)
-  # 
-  # re.w1<-enMap2(Example.Go.adjusted.by.exon,n=5,SimilarityThreshold=0)
-  # re.w2<-enMap2(Example.Go.unadjusted,n=5,SimilarityThreshold=0)
 }
