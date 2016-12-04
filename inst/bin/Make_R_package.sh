2ec56e46d2ebcb9c8580839bacd549512632a833
aiminy <jlfmssm@gmail.com>
2016-12-04 17:56:05 -0500

    add BioCheck bash script

---
+++ inst/bin/Make_R_package.sh
@@ -0,0 +1,5 @@
+R CMD build --resave-data PathwaySplice
+#tar -ztvf PathwaySplice_0.1.0.tar.gz
+#R CMD check --as-cran PathwaySplice_0.1.0.tar.gz
+R CMD BiocCheck PathwaySplice_0.0.1.tar.gz
+#R CMD INSTALL PathwaySplice_0.1.0.tar.gz
