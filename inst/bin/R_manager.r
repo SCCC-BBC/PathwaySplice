db153bddf716a0d1afbbb20b52de501d657510ca
aiminy <jlfmssm@gmail.com>
2016-12-10 15:34:39 -0500

    add case for Not and let user to select path

--- inst/bin/R_manager.r
+++ inst/bin/R_manager.r
@@ -1,5 +1,9 @@
 #!/usr/bin/Rscript 
 
+#Usage: 
+ 
+#Rscript  Rscript ~/PathwaySplice/inst/bin/R_manager.r
+
 cat("Do you want to build this package?\n")
 
 input<-file('stdin', 'r')
@@ -21,6 +25,9 @@ if(row=="Yes") {
     cat("Starting check as CRAN...\n")
     system("R CMD check --as-cran PathwaySplice_0.99.0.tar.gz")
     cat("Finished check as CRAN...\n")
+  }else{
+    cat("You decide Not check this package as CRAN at the moment\n")
+    quit()
   }
   
   cat("Do you want to perform BiocCheck for this package?\n")
@@ -32,8 +39,10 @@ if(row=="Yes") {
     cat("Starting BiocCheck...\n")
     system("R CMD BiocCheck PathwaySplice_0.99.0.tar.gz")
     cat("Finished BiocCheck...\n")
+  }else{
+    cat("You decide Not BiocCheck for this package at the moment\n")
+    quit()
   }
-  
   
   cat("Do you want to install this package?\n")
   
@@ -41,9 +50,25 @@ if(row=="Yes") {
   row <- readLines(input, n=1)
   
   if(row=="Yes"){
-    cat("Starting install...\n")
-    system("R CMD INSTALL PathwaySplice_0.99.0.tar.gz -l /Users/axy148/Library/R/3.3/library")
+    cat("please defne the library path from the following list:\n")
+    print(.libPaths())
+    
+    input<-file('stdin', 'r')
+    row <- readLines(input, n=1)
+    
+    if(row==1){
+      R_lib=.libPaths()[1]
+      cat(paste0("Starting install at ",R_lib,"...\n"))      
+      cmd=paste0("R CMD INSTALL PathwaySplice_0.99.0.tar.gz -l ",R_lib) 
+      system(cmd)
+    }
     cat("Finished install...\n")
+  }else{
+    cat("You decide Not install this package at the moment\n")
+    quit()
   }
   
+}else{
+  cat("You decide Not build this package at the moment\n")
+  quit()
 }
