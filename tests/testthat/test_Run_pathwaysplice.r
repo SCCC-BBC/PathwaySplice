796ff50a46c4908c6a94e13bdbf6226b2280321d
aiminy <jlfmssm@gmail.com>
2017-01-13 15:14:20 -0500

    change test1

--- tests/testthat/test_Run_pathwaysplice.r
+++ tests/testthat/test_Run_pathwaysplice.r
@@ -1,5 +1,11 @@
-data(mds)
-data(hg19)
+library(PathwaySplice)
+test_that("test Run_pathwaysplice",
+          {
+            data(mds)
+            data(hg19)
+            Example.Go.adjusted.by.exon<-Run_pathwaysplice(mds,ad='exon_SJ',sub_feature='E',
+                                                           0.05,genomeID='hg19',geneID='ensGene',gene_model=hg19,method='Wallenius')  
+          })
+  
 
-Example.Go.adjusted.by.exon<-Run_pathwaysplice(mds,ad='exon_SJ',sub_feature='E',
-0.05,genomeID='hg19',geneID='ensGene',gene_model=hg19,method='Wallenius')
+
