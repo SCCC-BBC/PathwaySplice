5541d0e5dac245a899e0e441520507f3ac1451c1
aiminy <jlfmssm@gmail.com>
2017-01-17 14:51:40 -0500

    comments out example

--- R/makeGeneWiseTable.R
+++ R/makeGeneWiseTable.R
@@ -12,22 +12,24 @@
 #'
 #' @examples
 #'
-#' all.gene.list<-unique(as.character(fData(Re.example)$geneID))
+#' #all.gene.list<-unique(as.character(fData(Re.example)$geneID))
 #' 
 #' #make a tiny example data set
-#' choosed.gene.list<-sample(all.gene.list,500)
-#' re.example.gene.based<-makeGeneWiseTable(Re.example,
-#' gene.list=choosed.gene.list)
-#' tiny.data<-re.example.gene.based
-#' save(tiny.data,file="./data/TinyData.RData")
+#' 
+#' #choosed.gene.list<-sample(all.gene.list,500)
+#' 
+#' #re.example.gene.based<-makeGeneWiseTable(Re.example,
+#' #gene.list=choosed.gene.list)
+#' #tiny.data<-re.example.gene.based
+#' #save(tiny.data,file="./data/TinyData.RData")
 #' 
 #' #make an example data using all genes
 #' 
-#' all.gene.data<-makeGeneWiseTable(Re.example,
-#' gene.list=all.gene.list)
-#' mds2<-all.gene.data
+#' #all.gene.data<-makeGeneWiseTable(Re.example,
+#' #gene.list=all.gene.list)
+#' #mds2<-all.gene.data
 #' 
-#' save(mds2,file="./data/mds2.RData")
+#' #save(mds2,file="./data/mds2.RData")
 #' 
 #'
 makeGeneWiseTable <- function(jscs, gene.list, FDR.threshold = 0.05, 
