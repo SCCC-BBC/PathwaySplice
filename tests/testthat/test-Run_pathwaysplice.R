1b3ac8f8254141ff17c643f4b17afc1b278f9999
aiminy <jlfmssm@gmail.com>
2017-03-26 23:59:38 -0400

    change to right function name

--- tests/testthat/test-Run_pathwaysplice.R
+++ tests/testthat/test-Run_pathwaysplice.R
@@ -1,7 +1,7 @@
 library(testthat)
-test_that("test LRtestBias",
+test_that("test lrtestbias",
           {
             data(mds11)
             mds33<-mds.11.sample[which(as.numeric(mds.11.sample$numExons)<=50),]
-            re<-LRtestBias(mds33,loc.x=2,loc.y=70,y_lim=80,boxplot_width=0.3,type="splicing")
+            re<-lrtestbias(mds33,loc.x=2,loc.y=70,y_lim=80,boxplot_width=0.3,type="splicing")
           })
