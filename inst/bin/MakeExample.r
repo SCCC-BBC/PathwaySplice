4c9b72e7f48afea13acc8ef61df2f8a56212db1b
aiminy <jlfmssm@gmail.com>
2017-04-03 11:26:36 -0400

    add feature for making small data set

--- inst/bin/MakeExample.r
+++ inst/bin/MakeExample.r
@@ -1,11 +1,11 @@
 
+#Generate a small annotation and count data set with 10 genes
+
 cmd0 <- "awk '{print $10}' ~/PathwaySplice/inst/extdata/GTF_Files/flat.chr22.gff | sort -u"
 
 gene.name <- try(system(cmd0,intern =TRUE))
 
 n <- 10
-
-# "awk -v s1="ENSG00000283023;" -v s2="ENSG00000283047;" '$10==s1 || $10==s2 {print}' PathwaySplice/inst/extdata/GTF_Files/flat.chr22.gff"
 
 cmd1 <- paste0("awk -v s1=","'",gene.name[1],"'")
 
@@ -15,32 +15,23 @@ cmd3 <- "~/PathwaySplice/inst/extdata/GTF_Files/flat.chr22.gff"
 
 cmd4 <- paste0("> ~/PathwaySplice/inst/extdata/GTF_Files/flat.chr22.",n,".genes.gff")
 
-cmd11 <- paste(cmd1,cmd2,cmd3,cmd4)
+cmd5 <- paste(cmd1,cmd2,cmd3,cmd4)
 
-system(cmd11)
-cmd5 <- paste0(">> ~/PathwaySplice/inst/extdata/GTF_Files/flat.chr22.",n,".genes.gff")
+system(cmd5)
+cmd6 <- paste0(">> ~/PathwaySplice/inst/extdata/GTF_Files/flat.chr22.",n,".genes.gff")
 
 lapply(2:length(gene.name),function(u,gene.name,n){
   
   if(u <= n){
     
-  cmd.temp <- paste0("awk -v s1=","'",gene.name[u],"'")
+  cmd7 <- paste0("awk -v s1=","'",gene.name[u],"'")
   
-  cmd12 <- paste(cmd.temp,cmd2,cmd3,cmd5)
+  cmd8 <- paste(cmd7,cmd2,cmd3,cmd6)
 
-  system(cmd12)
+  system(cmd8)
   }
   
 },gene.name,n)
-
-
-# t2 <- try(system(cmd,intern =TRUE))
-# 
-# gene.name <- "ENSG00000283047"
-# 
-# sh_cmd1 <- "grep ENSG00000283047 PathwaySplice/inst/extdata/GTF_Files/flat.chr22.gff > PathwaySplice/inst/extdata/GTF_Files/flat.chr22.one.gene.gff"
-# 
-# system(sh_cmd1)
 
 xx <- gene.name[1]
 
@@ -49,17 +40,17 @@ sample.list <- dir("~/PathwaySplice/inst/extdata/",pattern = "chr22")
 cmd.list <- lapply(sample.list,function(u,xx){
   
   cat(u,"\n")
-  cmd1 <- "grep"
-  cmd2 <- "~/PathwaySplice/inst/extdata/"
-  cmd3 <- "/QC.spliceJunctionAndExonCounts.forJunctionSeq.txt"
-  cmd4 <- ">"
-  cmd5 <- paste0("/Counts.",n,".genes.txt")
+  cmd9 <- "grep"
+  cmd10 <- "~/PathwaySplice/inst/extdata/"
+  cmd11 <- "/QC.spliceJunctionAndExonCounts.forJunctionSeq.txt"
+  cmd12 <- ">"
+  cmd13 <- paste0("/Counts.",n,".genes.txt")
   xxx <- gsub(";","",xx)
-  cmd <- paste(cmd1,xxx,paste0(cmd2,u,cmd3),cmd4,paste0(cmd2,u,cmd5),sep = " ")
+  cmd14 <- paste(cmd9,xxx,paste0(cmd10,u,cmd11),cmd12,paste0(cmd10,u,cmd13),sep = " ")
   
-  system(cmd)
+  system(cmd14)
   
-  cmd
+  cmd14
   
 },xx)
 
@@ -75,17 +66,17 @@ lapply(2:length(gene.name),function(x,gene.name,sample.list,n){
   cmd.list <- lapply(sample.list,function(y,xx){
     
     cat(y,"\n")
-    cmd1 <- "grep"
-    cmd2 <- "~/PathwaySplice/inst/extdata/"
-    cmd3 <- "/QC.spliceJunctionAndExonCounts.forJunctionSeq.txt"
-    cmd4 <- ">>"
-    cmd5 <- paste0("/Counts.",n,".genes.txt")
+    cmd15 <- "grep"
+    cmd16 <- "~/PathwaySplice/inst/extdata/"
+    cmd17 <- "/QC.spliceJunctionAndExonCounts.forJunctionSeq.txt"
+    cmd18 <- ">>"
+    cmd19 <- paste0("/Counts.",n,".genes.txt")
     xxx <- gsub(";","",xx)
-    cmd <- paste(cmd1,xxx,paste0(cmd2,y,cmd3),cmd4,paste0(cmd2,y,cmd5),sep = " ")
+    cmd20 <- paste(cmd15,xxx,paste0(cmd16,y,cmd17),cmd18,paste0(cmd16,y,cmd19),sep = " ")
     
-    system(cmd)
+    system(cmd20)
     
-    cmd
+    cmd20
     
   },xx)
   
@@ -95,14 +86,13 @@ lapply(2:length(gene.name),function(x,gene.name,sample.list,n){
   
 },gene.name,sample.list,n)
 
-# sh_cmd2 <- "grep ENSG00000283047 PathwaySplice/inst/extdata/"
-# 
-# SRR1660308_chr22
-# "/QC.spliceJunctionAndExonCounts.forJunctionSeq.txt > PathwaySplice/inst/extdata/SRR1660308_chr22/"
-# 
-# "QC.spliceJunctionAndExonCounts.forJunctionSeq.one.gene.txt"
-# 
-# system(sh_cmd2)
+cmd21 <- paste(paste0("head -",n),"~/PathwaySplice/inst/extdata/c2.cp.v5.2.symbols.gmt.txt",sep=" ")
 
+cmd22 <- " > ~/PathwaySplice/inst/extdata/10.cp.gmt.txt" 
 
+cmd23 <-paste(cmd21,cmd22)
+
+cmd23
+
+system(cmd23)
 
