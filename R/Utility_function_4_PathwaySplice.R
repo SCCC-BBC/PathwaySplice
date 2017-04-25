e3e8af9aba889eb36ed9c984146ac92dc4a3fd70
aiminy <jlfmssm@gmail.com>
2017-04-25 17:46:47 -0400

    move reformatpath function here

--- R/Utility_function_4_PathwaySplice.R
+++ R/Utility_function_4_PathwaySplice.R
@@ -852,3 +852,37 @@ match2Genome <- function(genome_id)
   })
   return(yyy)
 }
+
+#' reformatpath
+#' 
+#' @param dir.name Directory name to be converted 
+#' @return A converted directory
+#' 
+#' @examples
+#' dir.name <- '/media/H_driver/2016/Yang/MACS/MACS/'
+#' converted.dir.name <- PathwaySplice:::reformatpath(dir.name)
+#' 
+#' @export 
+
+reformatpath <- function(dir.name)
+{
+  CheckOPS <- Sys.info()[["sysname"]]
+  
+  if (CheckOPS == "Darwin")
+  {
+    temp <- unlist(strsplit(dir.name, split = "\\/"))
+    
+    if (!is.na(temp[3] == "H_driver"))
+    {
+      if (temp[3] == "H_driver")
+      {
+        temp[2] <- "Volumes"
+        temp[3] <- "Bioinformatics$"
+        dir.name <- do.call("file.path", as.list(temp))
+      }
+    }
+    
+  }
+  
+  return(dir.name)
+}
