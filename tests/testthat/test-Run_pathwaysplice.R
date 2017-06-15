0f84ba84dd9d088c89d1d4bef21f5a9a6fe3f49f
aiminy <jlfmssm@gmail.com>
2017-06-15 14:04:27 -0400

    add test compareResults

--- tests/testthat/test-Run_pathwaysplice.R
+++ tests/testthat/test-Run_pathwaysplice.R
@@ -14,3 +14,20 @@ test_that("test runPathwaySplice without size selection",
             gene.based.table <- makeGeneTable(featureBasedData)
             res <- runPathwaySplice(gene.based.table,genome='hg19',id='ensGene',test.cats=c('GO:BP'),method='Hypergeometric')
           })
+test_that("test compareResults",
+          {
+res1 <- runPathwaySplice(gene.based.table,genome='hg19',
+                         id='ensGene',test.cats=c('GO:BP'),
+                         go.size.cut=c(10,300),
+                         method='Wallenius')
+res2 <- runPathwaySplice(gene.based.table,genome='hg19',
+                         id='ensGene',test.cats=c('GO:BP'),
+                         go.size.cut=c(10,300),
+                         method='Hypergeometric')
+output.file.dir <- "~/TestNew"
+output.file.name.1 <- 'In_ad_not_un.xls'
+output.file.name.2 <- 'In_un_not_ad.xls'
+res3 <- compareResults(25,res1,res2,output.file.dir,output.file.dir,
+                       type.boxplot='Only3',
+                       output.file.name.1,output.file.name.2)
+})
